e41e37962f9a0fa02a944abcfdf27f636738688e
priska <priska@23bit.net>
2011-11-15 23:02:55 +0100

    again.

---
+++ src/test/java/de/tuberlin/dima/textmining/RunAssignment3.java
@@ -0,0 +1,9 @@
+package de.tuberlin.dima.textmining;
+
+import de.tuberlin.dima.textmining.assignment3.ShallowExtractorTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ShallowExtractorTest.class })
+public class RunAssignment3 {}
