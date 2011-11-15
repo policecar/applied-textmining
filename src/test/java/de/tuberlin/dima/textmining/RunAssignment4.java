4860ba672e322cd82526c7d15275c74a9dd2b1a4
priska <priska@23bit.net>
2011-11-16 00:16:56 +0100

    assignment 4, ready for another patch.

---
+++ src/test/java/de/tuberlin/dima/textmining/RunAssignment4.java
@@ -0,0 +1,9 @@
+package de.tuberlin.dima.textmining;
+
+import de.tuberlin.dima.textmining.assignment4.OpenInformationExtractorTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ OpenInformationExtractorTest.class })
+public class RunAssignment4 {}
