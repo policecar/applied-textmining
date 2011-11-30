3105aea3f2c55ff1debe0a188a4ab1f890748c60
priska <priska@23bit.net>
2011-11-30 17:56:39 +0100

    assignment 6.

--- src/main/java/de/tuberlin/dima/textmining/assignment6/WordReplacement.java
+++ src/main/java/de/tuberlin/dima/textmining/assignment6/WordReplacement.java
@@ -2,31 +2,68 @@ package de.tuberlin.dima.textmining.assignment6;
 
 import com.google.common.collect.Lists;
 import rita.wordnet.RiWordnet;
-
 import java.util.List;
 
 public class WordReplacement {
-	
+
+	private RiWordnet wordnet = new RiWordnet();
+
 	public Iterable<ShallowSentence> findWords(Iterable<ShallowSentence> sentences, int replaceInterval) {
 
-    List<ShallowSentence> words = Lists.newArrayList();
-		// TODO: implement a method, which replaces words in a sentence based 
-		//on a synonym mapping to Wordnet
+		List<ShallowSentence> words = Lists.newArrayList();
+		
+		// implement a method, which replaces words in a sentence based 
+		// on a synonym mapping to Wordnet
 
-		return words;
+		// NOTE: words replaced by a synonym are marked with <synonym>...</synonym>	
+		for ( ShallowSentence sentence : sentences ) {
+
+			int i = replaceInterval - 2;
+			String[] synonyms = null;
+			ShallowToken token = new ShallowToken("");
+			
+			while ( ( synonyms == null || synonyms.length == 0 ) && i < sentence.size()-1 ) {
+				i += 1;
+				token = sentence.get(i); //.elementAt(i);
+				synonyms = getSynonyms( token.getLemma(), token.getTag() );
+			}
+			if ( synonyms != null && synonyms.length != 0 ) {
+				int r = (int) ( Math.random() * synonyms.length );
+				// verb hack for 3rd person singulifying
+				if ( token.getTag().startsWith("VB") && token.getText().endsWith("s") 
+						&& !token.getText().endsWith("ss") ) {
+					synonyms[r] = synonyms[r] + "s";
+				}
+				// substitute the picked synonym into this sentence
+				token.setText( "<synonym>" + synonyms[r] + "</synonym>" );
+				sentence.set( i, token ); // apparently not needed
+			}
+			// NOTE: the next line makes the program freaking slow, hence substituted it
+//			words.add( sentence );
+			for ( ShallowToken tkn : sentence ) {
+				System.out.print( tkn.getText() + " ");
+			}
+			System.out.println();
+		}
+		return words;		
 	}
 	
 	/**
-	 * Generates all synonyms for a given word
+	 * Generates all synonyms for a given word and POS tag
 	 *
 	 * @return array of synonyms  
 	 */
-	public String[] getSynonyms(String word){
+	public String[] getSynonyms( String lemma, String tag ) {
 		
-		RiWordnet wordnet = new RiWordnet();
-		String[] synonyms = wordnet.getAllSynonyms(word, "n", 15);
-		
-		return synonyms;		
+		if ( tag.startsWith( "JJ" ) ) {
+			tag = "a";
+		} else if ( tag.startsWith( "RB" ) || tag.matches( "WRB" ) ) {
+			tag = "r";
+		} else if ( tag.startsWith( "VB" ) ) {
+			tag = "v";
+		} else {
+			tag = "n";
+		}
+		return wordnet.getAllSynonyms( lemma, tag, 15 );
 	}
-
 }
---
+++ src/test/java/de/tuberlin/dima/textmining/RunAssignment6.java
@@ -0,0 +1,9 @@
+package de.tuberlin.dima.textmining;
+
+import de.tuberlin.dima.textmining.assignment6.WordReplacementTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ WordReplacementTest.class })
+public class RunAssignment6 {}
